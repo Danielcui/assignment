32bad8d3deb49387d5f069f2819403e8221e69c3
Danielcui <13102399d@connect.polyu.hk>
2016-08-26 11:59:21 +0800

    assignment

---
+++ cachematrix.R
@@ -0,0 +1,26 @@
+##to create the function that cashe the computed value
+makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+}
+##to judge whether the value has been computed, calculated only if not previously caculated
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
+}
